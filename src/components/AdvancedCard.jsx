3a45cc13cbbfe7524440e81f3f5fc43f2f738d6d
Giacomo202210029 <u202210029@upc.edu.pe>
2025-02-08 22:52:18 -0500

    feat: Fixed array in AdvancedCard

--- src/components/AdvancedCard.jsx
+++ src/components/AdvancedCard.jsx
@@ -1,28 +1,40 @@
-
 import React from 'react';
 import { Card } from 'primereact/card';
 import { Button } from 'primereact/button';
 import { Tag } from 'primereact/tag';
+import {Galleria} from "primereact/galleria";
+
 
 export default function AdvancedCard(props) {
+    const images = [
+        { itemImageSrc: 'https://primefaces.org/cdn/primereact/images/galleria/galleria1.jpg', alt: 'Image 1' },
+        { itemImageSrc: 'https://primefaces.org/cdn/primereact/images/galleria/galleria2.jpg', alt: 'Image 2' },
+        { itemImageSrc: 'https://primefaces.org/cdn/primereact/images/galleria/galleria3.jpg', alt: 'Image 3' }
+    ];
+
+    const itemTemplate = (item) => {
+        return <img src={item.itemImageSrc} alt={item.alt} style={{ width: '100%', display: 'block' }} />;
+    };
+
     const header = (
-        <img alt="Card" src="https://primefaces.org/cdn/primereact/images/usercard.png" />
+        <Galleria value={images} item={itemTemplate} showItemNavigators showThumbnails={false} style={{ height: '16rem' }} />
     );
+
     const footer = (
-        <>
+        <div className="card-footer">
             <Button label="Ver Más" icon="pi pi-check" />
-        </>
+        </div>
     );
 
     return (
         <div className="card flex justify-content-center">
-            <Card title={props.name}  footer={footer} header={header} className="md:w-25rem custom-card-height">
-                <p className="m-0">
-                    {props.description}
+            <Card title={props.name}  footer={footer} header={header} className="card">
+                <p className="tecnologias">
                     {props.technology.map((tech, index) => (
-                        <Tag key={index} severity={props.color} value={tech}></Tag>
+                        <Tag  key={index} className="tag" style={{ backgroundColor: tech.color }} value={tech.name}></Tag>
                     ))}
                 </p>
+                <p>{props.description}</p>
             </Card>
         </div>
     )
