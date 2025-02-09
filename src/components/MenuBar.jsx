27493e9712b35f2ab57344ee2ff34b09b5113296
Giacomo202210029 <u202210029@upc.edu.pe>
2025-02-08 22:54:05 -0500

    feat: Added a Menu Bar

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -1,49 +1,39 @@
-
 import React from 'react';
-import { Menubar } from 'primereact/menubar';
+import {Menubar} from 'primereact/menubar';
+import {Button} from 'primereact/button';
+import logo from '../assets/logo.png'; // Asegúrate de ajustar la ruta al logo
 
 export default function BasicDemo() {
     const items = [
         {
             label: 'Sobre Mí',
-            icon: 'pi pi-home'
+            icon: 'pi pi-home',
+            className: 'menubar-text'
         },
         {
             label: 'Mi Experiencia',
-            icon: 'pi pi-star'
-
+            icon: 'pi pi-star',
+            className: 'menubar-text'
         },
         {
             label: 'Proyectos',
             icon: 'pi pi-search',
-            items: [
-                {
-                    label: 'Components',
-                    icon: 'pi pi-bolt'
-                },
-                {
-                    label: 'Blocks',
-                    icon: 'pi pi-server'
-                },
-                {
-                    label: 'UI Kit',
-                    icon: 'pi pi-pencil'
-                },
-                {
-                    label: 'Templates',
-                    icon: 'pi pi-palette',
-                }
-            ]
+            className: 'menubar-text'
         },
         {
-            label: 'Contact',
-            icon: 'pi pi-envelope'
+            label: 'Contáctame',
+            icon: 'pi pi-envelope',
+            className: 'menubar-text'
         }
     ];
 
+    const start = <img alt="logo" src={logo} style={{height: '40px'}}/>;
+    const end = <Button label="Mi Botón" icon="pi pi-check"/>;
+
     return (
-        <div className="card">
-            <Menubar model={items} />
+        <div className="menubar">
+            <Menubar model={items} start={start} end={end} className="menubar"/>
         </div>
     )
 }
+
