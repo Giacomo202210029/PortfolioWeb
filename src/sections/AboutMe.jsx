5bed67346075b50a3ff4867c417b66f53399bbcb
Giacomo202210029 <u202210029@upc.edu.pe>
2025-02-08 22:52:03 -0500

    feat: Fixed styles of About Me Section

--- src/sections/AboutMe.jsx
+++ src/sections/AboutMe.jsx
@@ -3,14 +3,18 @@
 const AboutMe = (props) => {
     const { name } = props //desestructura los props
     return (
-        <div>
-            <h1>Hola! Soy {name} </h1>
-            <h2>Ingeniero de Software</h2>
-            <p>📍Lima, Perú</p>
-            <p> +6 meses de experiencia como desarrollador Full Stack y actual estudiante en la
+        <div className="todoeltexto">
+            <h1 className="saludo">
+                <span className="highlight">Hola! </span>
+                <span className="spacing">Me llamo</span>
+            </h1>
+            <h1 className="nombre">{name} </h1>
+            <h3 className="carrera">Ingeniero de Software</h3>
+            <p className="ubicación">📍Lima, Perú</p>
+            <p className="presentacion"> +6 meses de experiencia como desarrollador Full Stack y actual estudiante en la
                 Universidad Peruana de Ciencias Aplicadas. He liderado equipos de desarrollo, amo aprender
                 constantemente
-                y siempre estoy dispuesto a dar todo en mi en los trabajos encomedados.</p>
+                y siempre estoy dispuesto a dar todo de mi en los trabajos encomedados.</p>
         </div>
     );
 
